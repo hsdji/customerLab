a731494030a1945b7653e0629b9aa86bac44d307
单小飞 <shanpengfei@duolaimei.net>
2018-12-24 21:55:42 +0800

    定位/名片 代完成

---
+++ customerLab/customerLab/wqrer.h
@@ -0,0 +1,17 @@
+//
+//  wqrer.h
+//  customerLab
+//
+//  Created by spf on 2018/12/24.
+//  Copyright © 2018年 spf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface wqrer : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
